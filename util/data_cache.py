69fe70eba59b235c1795fd09a79616c0a0c3d460
lintingbin2009 <331046161@qq.com>
2017-09-19 22:59:14 +0800

    All functions support sqlite3 db.

--- util/data_cache.py
+++ util/data_cache.py
@@ -1,19 +1,20 @@
-import os, fnmatch, re, threading, sublime, json, sqlite3
+import os, fnmatch, re, threading, sublime, json, sqlite3, shutil, time
 from .settings import get_settings_param, GLOBAL_SET
 
 CREATE_LIBS_SQL = '''
-CREATE TABLE IF NOT EXISTS `libs` (
-  `mod_name` VARCHAR(128) NOT NULL,
-  `fun_name` VARCHAR(128) NOT NULL,
-  `param_len` tinyint(2) NOT NULL,
-  `file_path` VARCHAR(256) NOT NULL,
-  `completion` VARCHAR(256) NOT NULL,
-  PRIMARY KEY (`mod_name`, `fun_name`, `param_len`)
+create table if not exists `libs` (
+  `id` integer PRIMARY KEY autoincrement,
+  `mod_name` varchar(128) not null,
+  `fun_name` varchar(128) not null,
+  `param_len` tinyint(2) not null,
+  `file_path` varchar(256) not null,
+  `row_num` int unsigned not null,
+  `completion` varchar(256) not null
 ); 
 '''
 INSERT_LIBS_SQL = '''
-REPLACE INTO libs(mod_name, fun_name, param_len, file_path, completion) VALUES 
-(?, ?, ?, ?, ?);
+replace into libs(mod_name, fun_name, param_len, file_path, row_num, completion) values 
+(?, ?, ?, ?, ?, ?);
 '''
 
 QUERY_COMPLETION = '''
@@ -24,26 +25,41 @@ QUERY_ALL_MOD = '''
 select distinct mod_name from libs;
 '''
 
+QUERY_POSITION = '''
+select fun_name, param_len, file_path, row_num from libs where mod_name = ? and fun_name = ?
+'''
+
 class DataCache:
     def __init__(self, dir = '', data_type = '', cache_dir = ''):
         self.dir = dir
-        self.libs = {}
-        self.fun_position = {}
-        self.modules = []
         self.data_type = data_type
         self.cache_dir = cache_dir
         self.re_dict = GLOBAL_SET['compiled_re']
         self.version = get_settings_param('sublime_erlang_version', '0.0.0')
+        self.is_ready = False
         if cache_dir != '':
-            self.init_db()
+            self.__init_db()
 
-    def init_db(self):
-        db_path = self.__get_filepath(self.data_type)
-        self.db_con = sqlite3.connect(db_path, check_same_thread = False)
-        self.db_cur = self.db_con.cursor()
-        self.db_cur.execute(CREATE_LIBS_SQL)
+    def __init_db(self):
+        db_path = self.__get_filepath('completion')
+        try:
+            self.db_con = sqlite3.connect(db_path, check_same_thread = False)
+            self.db_cur = self.db_con.cursor()
+            self.db_cur.execute(CREATE_LIBS_SQL)
+        except Exception as e:
+            self.db_con.close()
+            print('Exception', e)
+            shutil.rmtree(self.cache_dir)
+            print('Remove dir {}.'.format(self.cache_dir))
+            db_path = self.__get_filepath('completion')
+            self.db_con = sqlite3.connect(db_path, check_same_thread = False)
+            self.db_cur = self.db_con.cursor()
+            self.db_cur.execute(CREATE_LIBS_SQL)
 
     def query_mod_fun(self, module):
+        if not self.is_ready:
+            return []
+
         self.db_cur.execute(QUERY_COMPLETION, (module, ))
         query_data = self.db_cur.fetchall()
 
@@ -54,12 +70,28 @@ class DataCache:
         return completion_data
 
     def query_all_mod(self):
+        if not self.is_ready:
+            return []
+        
         self.db_cur.execute(QUERY_ALL_MOD)
         query_data = self.db_cur.fetchall()
 
         completion_data = []
         for (mod_name, ) in query_data:
             completion_data.append(['{}\tModule'.format(mod_name), mod_name])
+
+        return completion_data
+
+    def query_fun_position(self, module, function):
+        if not self.is_ready:
+            return []
+
+        self.db_cur.execute(QUERY_POSITION, (module, function))
+        query_data = self.db_cur.fetchall()
+
+        completion_data = []
+        for (fun_name, param_len, file_path, row_num) in query_data:
+            completion_data.append(('{}/{}'.format(fun_name, param_len), file_path, row_num))
 
         return completion_data
 
@@ -75,7 +107,7 @@ class DataCache:
                     export_fun[(name, int(cnt))] = None
             module = self.get_module_from_path(filepath)
 
-            row_id = 1
+            row_num = 1
             for line in code.split('\n'):
                 funhead = self.re_dict['funline'].search(line)
                 if funhead is not None: 
@@ -86,8 +118,8 @@ class DataCache:
                     if (fun_name, param_len) in export_fun:
                         del(export_fun[(fun_name, param_len)])
                         completion = self.__tran2compeletion(fun_name, param_list, param_len)
-                        self.db_cur.execute(INSERT_LIBS_SQL, (module, fun_name, param_len, filepath, completion))
-                row_id += 1
+                        self.db_cur.execute(INSERT_LIBS_SQL, (module, fun_name, param_len, filepath, row_num, completion))
+                row_num += 1
 
     def get_module_from_path(self, filepath):
         (path, filename) = os.path.split(filepath)
@@ -131,13 +163,17 @@ class DataCache:
 
         for filepath in all_filepath:
             self.build_module_dict(filepath)
+        
         self.db_con.commit()
 
     def build_data_async(self):
         this = self
         class BuildDataAsync(threading.Thread):
             def run(self):
-                # this.build_data()
-                True
+                start_time = time.time()
+                print("start", start_time)
+                this.build_data()
+                print("end", time.time() - start_time)
+                this.is_ready = True
                 
         BuildDataAsync().start()
--- util/go_to.py
+++ util/go_to.py
@@ -1,12 +1,21 @@
 from .data_cache import DataCache
+from functools import partial
+from html import escape
 import sublime, re, os
 
 class GoTo(DataCache):
     def __init__(self):
         DataCache.__init__(self)
-        self.max_col = 400
+        self.__max_col = 400
+        self.__line_style = 'margin:5 0'
+        self.__definition_style = 'font-weight:bold;font-size:20;margin:5 0'
 
-    def run(self, point, view, cache):
+    def run(self, point, view, cache, is_quick_panel = False):
+        self.__view = view
+        self.__point = point
+        self.__window = view.window()
+        self.__is_quick_panel = is_quick_panel
+
         line_region = view.line(point)
         line_str = view.substr(line_region)
         word_region = view.word(point)
@@ -16,76 +25,82 @@ class GoTo(DataCache):
         maths = self.re_dict['take_mf'].findall(line_str)
         for math in maths:
             if word in math:
-                if self.open_function_popup(view, point, math[0], math[1], cache['libs'].fun_position):
+                if self.__goto_menu(view, cache['libs'].query_fun_position(math[0], math[1])):
                     return
-                if self.open_function_popup(view, point, math[0], math[1], cache['project'].fun_position):
+                if self.__goto_menu(view, cache['project'].query_fun_position(math[0], math[1])):
                     return
 
         maths = self.re_dict['take_fun'].findall(line_str)
         for math in maths:
             if word == math:
                 module = self.get_module_from_path(filepath)
-                if self.open_function_popup(view, point, 'erlang', math, cache['libs'].fun_position):
+                if self.__goto_menu(view, cache['libs'].query_fun_position('erlang', math)):
                     return
-                if self.open_function_popup(view, point, module, math, self.build_module_position(view, module, filepath)):
+                if self.__goto_menu(view, self.__build_module_position(view, module, math, filepath)):
                     return
 
         maths = self.re_dict['take_record'].findall(line_str)
         for math in maths:
             if word == math:
                 re_define = re.compile(r'(-\s*record\s*\([\s\n\r]*' + word + r'[\s\n\r]*,[\s\n\r]*{[^-]*}[\s\n\r]*\)\.)', re.MULTILINE|re.DOTALL)
-                if self.open_hrl_popup(view, re_define, filepath, point):
+                if self.__open_hrl_popup(view, re_define, filepath):
                     return
 
         maths = self.re_dict['take_define'].findall(line_str)
         for math in maths:
             if word == math:
                 re_define = re.compile(r'(-\s*define\s*\([\s\n\r]*' + word + r'[\s\n\r]*,[\s\n\r]*[^-]*\)\.)', re.MULTILINE|re.DOTALL)
-                if self.open_hrl_popup(view, re_define, filepath, point):
+                if self.__open_hrl_popup(view, re_define, filepath):
                     return
 
-    def open_function_popup(self, view, point, module, function, all_function):
-        key = (module, function)
-        if key in all_function:
-            col = 0
-            row = 1
-            html_content = '<div>Definitions:</div>'
-            for (name, path, row) in all_function[key]:
-                add_str = '<div>{0}:{1} <a href="{2}:{3}:0">{2}:{3}</a></div>'.format(module, name, path, row)
-                html_content += add_str
-                col = max(len(add_str), col)
-                row += 1
+    def __goto_menu(self, view, data):
+        if data == []:
+            return False
 
-            view.show_popup(html_content, max_height = self.get_height(row), max_width = self.get_width(col), 
-                flags = sublime.HIDE_ON_MOUSE_MOVE_AWAY, location = point, 
-                on_navigate = self.on_navigate_cb)
-            return True
-        return False
+        if self.__is_quick_panel:
+            self.__window_quick_panel_open_window(data)
+        else:
+            self.__open_function_popup(view, data)
+        return True
 
-    def build_module_position(self, view, module, filepath):
-        row_id = 1
-        cur_view_position = {}
+    def __open_function_popup(self, view, data):
+        col = 0
+        row = 1
+        html_content = '<div style={}>Definitions:</div>'.format(self.__definition_style)
+        for (name, path, row) in data:
+            add_str = '<div style={0}>{1} <a href="{2}:{3}:0">{2}:{3}</a></div>'.format(self.__line_style, name, path, row)
+            html_content += add_str
+            col = max(len(add_str), col)
+            row += 1
+
+        view.show_popup(html_content, max_height = self.__get_height(row), max_width = self.__get_width(col), 
+            flags = sublime.HIDE_ON_MOUSE_MOVE_AWAY, location = self.__point, 
+            on_navigate = self.__on_navigate_cb)
+        return True
+
+    def __build_module_position(self, view, module, fun, filepath):
+        row_num = 1
+        cur_view_position = []
         all_cur_view_fun = []
-        code = self.get_view_code(view)
+        code = self.__get_view_code(view)
         for line in code.split('\n'):
             funhead = self.re_dict['funline'].search(line)
             if funhead is not None: 
                 fun_name = funhead.group(1)
-                param_str = funhead.group(2)
-                param_list = self.format_param(param_str)
-                key = (module, fun_name)
-                param_len = len(param_list)
-                format_fun_name = '{0}/{1}'.format(fun_name, param_len)
-                
-                if (key, param_len) not in all_cur_view_fun:
-                    if key not in cur_view_position:
-                        cur_view_position[key] = []
-                    cur_view_position[key].append((format_fun_name, filepath, row_id))
-                    all_cur_view_fun.append((key, param_len))
-            row_id += 1
+                if fun_name == fun:
+                    param_str = funhead.group(2)
+                    param_list = self.format_param(param_str)
+                    key = (module, fun_name)
+                    param_len = len(param_list)
+                    format_fun_name = '{0}/{1}'.format(fun_name, param_len)
+                    
+                    if (key, param_len) not in all_cur_view_fun:
+                        cur_view_position.append((format_fun_name, filepath, row_num))
+                        all_cur_view_fun.append((key, param_len))
+            row_num += 1
         return cur_view_position
 
-    def open_hrl_popup(self, view, re_define, filepath, point):
+    def __open_hrl_popup(self, view, re_define, filepath):
         if not os.path.exists(filepath):
             return False
         
@@ -96,41 +111,67 @@ class GoTo(DataCache):
         re_newline = re.compile(r'\n')
         for m in re_define.finditer(code):
             start_line = len(re_newline.findall(code, 0, m.start(1))) + 1
-            html_content = '<div>Definitions:</div>'
-            record_define = '<div>{0}</div>'.format(m.group(1))
-            reocrd_address = '<div><a href="{0}:{1}:0">{0}:{1}</a></div>'.format(filepath, start_line)
-            record_define_len = len(record_define)
-            if record_define_len > self.max_col:
-                html_content += reocrd_address
-                col = len(reocrd_address)
-            else:
-                html_content += record_define + reocrd_address
-                col = max(record_define_len, len(reocrd_address))
 
-            view.show_popup(html_content, max_height = self.get_height(3), max_width = self.get_width(col), 
-                flags = sublime.HIDE_ON_MOUSE_MOVE_AWAY, location = point, 
-                on_navigate = self.on_navigate_cb)
+            if self.__is_quick_panel:
+                self.__window_quick_panel_open_window([('', filepath, start_line)])
+            else:
+                html_content = '<div style={}>Definitions:</div>'.format(self.__definition_style)
+                record_define = '<div style={}>{}</div>'.format(self.__line_style, escape(m.group(1), quote = False))
+                record_address = '<div style={0}><a href="{1}:{2}:0">{1}:{2}</a></div>'.format(self.__line_style, filepath, start_line)
+                record_define_len = len(record_define)
+                if record_define_len > self.__max_col:
+                    html_content += record_address
+                    col = len(record_address)
+                else:
+                    html_content += record_define + record_address
+                    col = max(record_define_len, len(record_address))
+
+                view.show_popup(html_content, max_height = self.__get_height(3), max_width = self.__get_width(col), 
+                    flags = sublime.HIDE_ON_MOUSE_MOVE_AWAY, location = self.__point, 
+                    on_navigate = self.__on_navigate_cb)
             return True
         
         (directory, filename) = os.path.split(filepath)
         for m in self.re_dict['take_include'].finditer(code):
             hrl_name = m.group(1)
             hrl_path = os.path.normpath(os.path.join(directory, hrl_name))
-            if self.open_hrl_popup(view, re_define, hrl_path, point):
+            if self.__open_hrl_popup(view, re_define, hrl_path):
                 return True
                 
         return False
 
-    def on_navigate_cb(self, address):
+    def __on_navigate_cb(self, address):
         sublime.active_window().open_file(address, sublime.ENCODED_POSITION)
 
-    def get_view_code(self, view):
+    def __get_view_code(self, view):
         view_region = sublime.Region(0, view.size())
         code = view.substr(view_region)
         return code
 
-    def get_height(self, row):
+    def __window_quick_panel_open_window(self, options):
+        self.options = options
+
+        self.__window.show_quick_panel(
+            [self.__show_option(o) for o in options],
+            self.__jump_to_in_window,
+            on_highlight=partial(self.__jump_to_in_window))
+
+    def __jump_to_in_window(self, index, row_num = None, param_cnt = None):
+        if index == -1:  
+            self.__window.focus_view(self.__view)
+            self.__view.show(self.__point)
+            self.__view.sel().clear()
+            self.__view.sel().add(self.__point)
+            return
+
+        (fun_name, filepath, row_num) = self.options[index]
+        self.__window.open_file('{0}:{1}'.format(filepath, row_num or 0), sublime.ENCODED_POSITION|sublime.TRANSIENT)
+
+    def __show_option(self, options):
+        return '{0}: {1}:{2}'.format(*options)
+
+    def __get_height(self, row):
         return row * 240
 
-    def get_width(self, col):
+    def __get_width(self, col):
         return col * 20
